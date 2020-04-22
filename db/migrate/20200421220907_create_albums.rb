9076de0c555e62ccb0cede690b7df2ba836dba21
SWillames <sergiowoc@gmail.com>
2020-04-22 14:19:42 -0300

    Fixes in the migration file.

--- db/migrate/20200421220907_create_albums.rb
+++ db/migrate/20200421220907_create_albums.rb
@@ -2,7 +2,7 @@ class CreateAlbums < ActiveRecord::Migration[6.0]
   def change
     create_table :albums do |t|
       t.string :title
-      t.datime :date
+      t.datetime :date
       t.references :category, null: false, foreign_key: true
       t.references :artist, null: false, foreign_key: true
 
